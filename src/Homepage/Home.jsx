7e58d307e0edc994793cbdfa5c1d7712f0112827
Uma Shankar <cashdhell@gmail.com>
2026-01-02 19:36:29 +0530

    home page

---
+++ src/Homepage/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { FaHandPointRight } from "react-icons/fa6";
+import Signup from "./Signup";
+const Home = () => {
+  return (
+    <main className="grid grid-cols-2 overflow-hidden ">
+      <section className="home  ml-50 flex flex-col py-20  ">
+        <h1 className="logo text-[#d9ca26]">
+          R <span className="reqo">eqo</span>
+        </h1>
+        <h2 className="text-xl font-bold text-white">
+          Chat Only ,{" "}
+          <span className="text-[#95d6a4] text-2xl">When Both Agree....</span>
+        </h2>
+        <p className="text-md mt-5 font-semibold text-white ">
+          Reqo is a request-based chatting platform where conversations start
+          only after mutual acceptance. Send a chat request, wait for approval,
+          and talk only when the other person is ready. Simple. Respectful.
+          Controlled
+        </p>
+        <button className="mt-12 bg-[#d9ca26] font-medium text-2xl flex gap-5  items-center self-baseline py-2 px-6 rounded-md cursor-pointer hover:bg-[#95d6a4] hover:scale-105">
+          Let's talk today{" "}
+          <i className="text-3xl">
+            <FaHandPointRight />
+          </i>
+        </button>
+      </section>
+      <section className="py-4 mr-20 ">
+        <Signup />
+      </section>
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/Homepage/Signup.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className="  h-full grid items-center px-10">
+      <form
+        action=""
+        className=" form flex flex-col gap-8 py-3 px-20  bg-black border-[3px] border-[#d9ca26] rounded-2xl"
+      >
+        <div>
+          <h2>First Name</h2>
+          <input type="text" className="input" />
+        </div>
+        <div>
+          <h2>Last Name</h2>
+          <input type="text" className="input" />
+        </div>
+        <div>
+          <h2>Email</h2>
+          <input type="email" className="input" />
+        </div>
+        <div>
+          <h2>Phone Number</h2>
+          <input type="text" className="input" />
+        </div>
+        <div>
+          <h2>Password</h2>
+          <input type="text" className="input" />
+        </div>
+
+        <div className="flex place-items-center flex-col ">
+          <p className="text-white text-center text-sm ">
+            Already have an account ?{" "}
+            <a href="" className="text-red-600 font-semibold text-md">
+              Login
+            </a>
+          </p>
+          <button className=" bg-[#d9ca26] font-medium text-lg flex  py-1 px-4 rounded-md cursor-pointer hover:bg-[#95d6a4] hover:scale-105  mt-2">
+            Signup
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Signup
