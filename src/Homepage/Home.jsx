2b8b4b96107d9ecd9b08fc4635d6bd42aaa25a27
pradyumna-kumar-parida <pradyumnap584@gmail.com>
2026-01-03 00:51:05 +0530

    ui changes

--- src/Homepage/Home.jsx
+++ src/Homepage/Home.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaHandPointRight } from "react-icons/fa6";
 import Signup from "./Signup";
 const Home = () => {
+  const [click, setClick] = useState(false)
+  console.log("home", click);
+
+
   return (
     <main className="grid grid-cols-2 overflow-hidden ">
       <section className="home  ml-50 flex flex-col py-20  ">
-        <h1 className="logo text-[#d9ca26]">
+        <h1 className={`logo ${click ? "text-[#95d6a4]" : "text-[#d9ca26]"
+          }`}>
           R <span className="reqo">eqo</span>
         </h1>
         <h2 className="text-xl font-bold text-white">
           Chat Only ,{" "}
-          <span className="text-[#95d6a4] text-2xl">When Both Agree....</span>
+          <span className={`${!click ? "text-[#95d6a4]" : "text-[#d9ca26]"
+            } text-2xl`}>When Both Agree....</span>
         </h2>
         <p className="text-md mt-5 font-semibold text-white ">
           Reqo is a request-based chatting platform where conversations start
@@ -18,7 +24,8 @@ const Home = () => {
           and talk only when the other person is ready. Simple. Respectful.
           Controlled
         </p>
-        <button className="mt-12 bg-[#d9ca26] font-medium text-2xl flex gap-5  items-center self-baseline py-2 px-6 rounded-md cursor-pointer hover:bg-[#95d6a4] hover:scale-105">
+        <button className={`mt-12 ${click ? "bg-[#95d6a4]" : "bg-[#d9ca26]"
+          } font-medium text-2xl flex gap-5  items-center self-baseline py-2 px-6 rounded-md cursor-pointer active:bg-[#95d6a4] hover:scale-105`} onClick={() => setClick(!click)}>
           Let's talk today{" "}
           <i className="text-3xl">
             <FaHandPointRight />
@@ -26,7 +33,7 @@ const Home = () => {
         </button>
       </section>
       <section className="py-4 mr-20 ">
-        <Signup />
+        <Signup clicked={click} setclicked={setClick} />
       </section>
     </main>
   );
--- src/Homepage/Signup.jsx
+++ src/Homepage/Signup.jsx
@@ -1,31 +1,36 @@
 import React from 'react'
 
-const Signup = () => {
+const Signup = (props) => {
+  const { clicked, setclicked } = props
+  console.log("SIgn", clicked);
+
   return (
     <div className="  h-full grid items-center px-10">
       <form
         action=""
-        className=" form flex flex-col gap-8 py-3 px-20  bg-black border-[3px] border-[#d9ca26] rounded-2xl"
+        className={`form flex flex-col gap-8 py-3 px-20 bg-black border-[5px] ${clicked ? "border-[#95d6a4]" : "border-[#d9ca26]"
+          } rounded-2xl`}
+
       >
         <div>
           <h2>First Name</h2>
-          <input type="text" className="input" />
+          <input type="text" className={`${clicked ? "warn" : "input"}`} />
         </div>
         <div>
           <h2>Last Name</h2>
-          <input type="text" className="input" />
+          <input type="text" className={`${clicked ? "warn" : "input"}`} />
         </div>
         <div>
           <h2>Email</h2>
-          <input type="email" className="input" />
+          <input type="email" className={`${clicked ? "warn" : "input"}`} />
         </div>
         <div>
           <h2>Phone Number</h2>
-          <input type="text" className="input" />
+          <input type="text" className={`${clicked ? "warn" : "input"}`} />
         </div>
         <div>
           <h2>Password</h2>
-          <input type="text" className="input" />
+          <input type="text" className={`${clicked ? "warn" : "input"}`} />
         </div>
 
         <div className="flex place-items-center flex-col ">
@@ -35,7 +40,7 @@ const Signup = () => {
               Login
             </a>
           </p>
-          <button className=" bg-[#d9ca26] font-medium text-lg flex  py-1 px-4 rounded-md cursor-pointer hover:bg-[#95d6a4] hover:scale-105  mt-2">
+          <button className={`${clicked ? "bg-[#95d6a4]" : "bg-[#d9ca26]"} font-medium text-lg flex  py-1 px-4 rounded-md cursor-pointer hover:scale-105  mt-2`}>
             Signup
           </button>
         </div>
